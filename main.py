b3249d6f7350a9a7aa2ab2de399f9f92c91f8b7a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-10-05 03:19:31 -0400

    re-encode this entire thing

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from time import sleep
 #! SO YOU BEST NOT TYPE IN YOUR PASSWORD WHEN YOU'RE RUNNING THIS!!!
 
 # This is the server you're playing on. If it's local 127.0.0.1
-ip = "my.cool.server"
+ip = "127.0.0.1"
 ORIGIN_ip = "127.0.0.1"
 
 # This is the target (server) port. It will be used as a nand gate and mutated.
@@ -48,97 +48,125 @@ port = int(x)
 cache = []
 
 #* Type: str
-def fire():
+# def fire():
 
-  if len(cache) == 0:
-     return
+  # if len(cache) == 0:
+  #    return
 
   # pid         peer  chan
   #[0, 1, 2, 3][4, 5][6 ]
 
   #! HEADER 7 bytes.
 
-  # protocol ID
-  t = int("0x4f457403", 16)
-  w = bytearray()
-  w.extend(t.to_bytes(length=4, byteorder='big'))
+# protocol ID
+t = int("0x4f457403", 16)
+w = bytearray()
+w.extend(t.to_bytes(length=4, byteorder='big'))
 
-  # peer (is server)
-  w.extend((1).to_bytes(length=2, byteorder='big'))
+# peer (is server)
+w.extend((1).to_bytes(length=2, byteorder='big'))
 
-  # channel
-  w.extend((2).to_bytes(length=1, byteorder='big'))
+# channel
+w.extend((2).to_bytes(length=1, byteorder='big'))
 
-  #! Packet type.
+#! Packet type.
 
-  # type
-  w.extend((1).to_bytes(length=1, byteorder='big'))
+# type
+w.extend((1).to_bytes(length=1, byteorder='big'))
 
-  # control type
-  w.extend((0).to_bytes(length=1, byteorder='big'))
+# control type
+w.extend((0).to_bytes(length=1, byteorder='big'))
 
-  # Instruction - Mod channel function pointer is at 0x57
-  z = int("0x2F", 16)
-  w.extend(z.to_bytes(length=1, byteorder='big'))
+# Instruction - Mod channel function pointer is at 0x57
+z = int("0x57", 16)
+w.extend(z.to_bytes(length=1, byteorder='big'))
 
-  # Chat version
-  w.extend((1).to_bytes(length=1, byteorder='big'))
+# This is extremely complex, we're basically forcing alternate packet intake LOL.
 
-  # Message type
-  w.extend((0).to_bytes(length=1, byteorder='big'))
+# Encoding: (str_len + 1 [null term]) * 2 [probably]
 
-  # I honestly have no idea
-  w.extend((1).to_bytes(length=2, byteorder='big'))
-  w.extend((0).to_bytes(length=2, byteorder='big'))
+w.extend((14).to_bytes(length=2, byteorder='big'))
+w.extend(bytes("ultra_key_2000","utf-8"))
 
-  output = "-&-".join(cache)
+w.extend((14).to_bytes(length=2, byteorder='big'))
+w.extend(bytes("ultra_key_2000","utf-8"))
 
-  w.extend((len(output)).to_bytes(length=2, byteorder='big'))
+qw = "a test message"
 
-  # Pad this thing because it crashes for literally no reason
-  w.extend(bytes(output + "aaaaasfsadfdsfasfsffasfdsdf","utf-16be"))
+ll = (len(qw))
 
-  #* ConnectionReceiveThread::receive
-  #* ConnectionReceiveThread::processPacket
-
-  # print(ORIGIN_port)
-  # print(port)
-  # print(w)
-
-
-  _ip = scapy.IP(dst = "127.0.0.1", src = "127.0.0.1")
-  _udp = scapy.UDP(sport = ORIGIN_port, dport = port)
-
-  scapy.send(_ip/_udp/w)
-
-  cache.clear()
+w.extend(ll.to_bytes(length=2, byteorder='big'))
+w.extend(bytes(qw,"utf-8"))
 
 
 
-def OnKeyPress(event): 
-    cache.append('{}_down'.format(event.Key).lower())
+# w.extend((2).to_bytes(length=4, byteorder='big'))
+# w.extend(bytes("hi","utf-16be"))
+# w.extend(b'\x00')
 
-def OnKeyUp(event): 
-    cache.append('{}_up'.format(event.Key).lower())
+# w.extend(bytes("hi","utf-16be"))
 
-new_hook = pyxhook.HookManager() 
-new_hook.KeyDown = OnKeyPress 
-new_hook.KeyUp = OnKeyUp
+# w.extend(b'\x00')
 
 
-new_hook.HookKeyboard() 
-try: 
-    new_hook.start()
-except KeyboardInterrupt: 
-    pass
-except Exception as ex: 
-    msg = 'Error while catching events:\n  {}'.format(ex) 
-    pyxhook.print_err(msg) 
-    print("I suck at python :D")
+# Chat version
+# w.extend((1).to_bytes(length=1, byteorder='big'))
 
-while True:
-   sleep(delta)
-   fire()
+# Message type
+# w.extend((0).to_bytes(length=1, byteorder='big'))
+
+# I honestly have no idea
+# w.extend((1).to_bytes(length=2, byteorder='big'))
+# w.extend((0).to_bytes(length=2, byteorder='big'))
+
+# output = "-&-".join(cache)
+
+# w.extend((len(output)).to_bytes(length=2, byteorder='big'))
+
+# # Pad this thing because it crashes for literally no reason
+# w.extend(bytes(output + "aaaaasfsadfdsfasfsffasfdsdf","utf-16be"))
+
+#* ConnectionReceiveThread::receive
+#* ConnectionReceiveThread::processPacket
+
+# print(ORIGIN_port)
+# print(port)
+# print(w)
+
+
+_ip = scapy.IP(dst = ORIGIN_ip, src = ip)
+_udp = scapy.UDP(sport = ORIGIN_port, dport = port)
+
+scapy.send(_ip/_udp/w)
+
+# cache.clear()
+
+
+
+# def OnKeyPress(event): 
+#     cache.append('{}_down'.format(event.Key).lower())
+
+# def OnKeyUp(event): 
+#     cache.append('{}_up'.format(event.Key).lower())
+
+# new_hook = pyxhook.HookManager() 
+# new_hook.KeyDown = OnKeyPress 
+# new_hook.KeyUp = OnKeyUp
+
+
+# new_hook.HookKeyboard() 
+# try: 
+#     new_hook.start()
+# except KeyboardInterrupt: 
+#     pass
+# except Exception as ex: 
+#     msg = 'Error while catching events:\n  {}'.format(ex) 
+#     pyxhook.print_err(msg) 
+#     print("I suck at python :D")
+
+# while True:
+#    sleep(delta)
+#    fire()
    
 
     
